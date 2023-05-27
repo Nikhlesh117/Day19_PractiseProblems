be6d3383c307fc7aba148cc04bf9f0a3ee7163d0
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-05-27 18:06:13 +0530

    Added: Insert Values

--- Customer.sql
+++ Customer.sql
@@ -1,8 +1,8 @@
---UC1_Create_Database
+--UC1_Create Database
 create database CustomerDetail;
 use CustomerDetail;
 
---UC2_Create_Customer_Table
+--UC2_Create Customer Table
 CREATE TABLE Customer
 (
     CustomerId INT IDENTITY(1,1) PRIMARY KEY,
@@ -10,5 +10,11 @@ CREATE TABLE Customer
     Phone VARCHAR(20),
     Address VARCHAR(255)
 );
+
+--UC3 Insert Values
+INSERT INTO Customer (CustomerName, Phone, Address)
+VALUES ('John Doe', '1234567890', '123 Main St'),
+('Jane Smith', '9876543210', '456 Elm St'),
+('Mike Johnson', '5551234567', '789 Oak Ave');
 
 select * from Customer;
