fa04e435b31d0b7008120d4ce7c987ca28bd2740
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-05-27 18:13:19 +0530

    Added: Add New Column

--- Customer.sql
+++ Customer.sql
@@ -17,4 +17,15 @@ VALUES ('John Doe', '1234567890', '123 Main St'),
 ('Jane Smith', '9876543210', '456 Elm St'),
 ('Mike Johnson', '5551234567', '789 Oak Ave');
 
+--UC4 Add New Column in Existing table
+-- Add new columns to the Customer table
+ALTER TABLE Customer
+ADD Country VARCHAR(100),
+    Salary DECIMAL(10,2),
+    Pincode VARCHAR(10);
+-- Update values for the new columns
+UPDATE Customer SET Country = 'USA', Salary = 5000.00, Pincode = '12345' WHERE CustomerId = 1;
+UPDATE Customer SET Country = 'Canada', Salary = 4000.00, Pincode = '54321' WHERE CustomerId = 2;
+UPDATE Customer SET Country = 'UK', Salary = 6000.00, Pincode = '98765' WHERE CustomerId = 3;
+
 select * from Customer;
