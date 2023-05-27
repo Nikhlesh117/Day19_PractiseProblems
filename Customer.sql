f838a267eb17691117e6efc42ccc3131e0968c5d
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-05-27 18:02:44 +0530

    Added: Create Table

--- Customer.sql
+++ Customer.sql
@@ -1,3 +1,14 @@
 --UC1_Create_Database
 create database CustomerDetail;
 use CustomerDetail;
+
+--UC2_Create_Customer_Table
+CREATE TABLE Customer
+(
+    CustomerId INT IDENTITY(1,1) PRIMARY KEY,
+    CustomerName VARCHAR(255),
+    Phone VARCHAR(20),
+    Address VARCHAR(255)
+);
+
+select * from Customer;
